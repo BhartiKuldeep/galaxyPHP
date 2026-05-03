06bfd5fb52879c95364acf977ea309c6c4e61a3c
Bharti Kuldeep <bhartikuldeep2001@rediffmail.com>
2026-05-03 10:53:31 +0530

    Add custom 404 error page

---
+++ resources/views/errors/404.php
@@ -0,0 +1,7 @@
+<section class="hero" style="grid-template-columns:1fr;">
+    <div class="card">
+        <h1>404</h1>
+        <p class="muted">The page you requested could not be found.</p>
+        <a class="btn btn-primary" href="/">Go home</a>
+    </div>
+</section>
