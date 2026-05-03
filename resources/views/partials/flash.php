043dde6b8b151ceabccbc2a06c76ca8bb2cee261
Bharti Kuldeep <bhartikuldeep2001@rediffmail.com>
2026-05-03 10:56:40 +0530

    Add flash message partial for success and error alerts

---
+++ resources/views/partials/flash.php
@@ -0,0 +1,7 @@
+<?php if ($message = flash('success')): ?>
+    <div class="alert alert-success"><?= e($message) ?></div>
+<?php endif; ?>
+
+<?php if ($message = flash('error')): ?>
+    <div class="alert alert-error"><?= e($message) ?></div>
+<?php endif; ?>
